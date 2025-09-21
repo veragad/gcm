d9459a0d7a025c4da6087b1aa502bc0de58b76d0
Vera <143955529+veragad@users.noreply.github.com>
2025-09-21 15:27:47 -0300

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        int numero = new Random().nextInt(5) + 1; // número de 1 a 5
+        System.out.print("Adivinhe um número de 1 a 5: ");
+        int palpite = new Scanner(System.in).nextInt();
+        System.out.println(palpite == numero ? "🎉 Acertou!" : "❌ Errou! O número era " + numero);
+    }
+}
